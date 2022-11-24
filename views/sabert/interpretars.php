e9a705ae56407e934d54f919444dd6c8cef04d55
Anibal Benitez <hababel@gmail.com>
2022-11-24 11:21:59 -0500

    .

--- views/sabert/interpretars.php
+++ views/sabert/interpretars.php
@@ -10,7 +10,10 @@
        <div class="row">
          <div class="col">
            <div class="card-body" style="height:1100px;">
-              <object data="<?php echo URL_PATH;?>views/pdfs/Info_nal_resultadospruebassaberpro_20202125.pdf" height="100%" width="100%"></object>
+              <!-- <object data="<?php //echo URL_PATH;?>views/pdfs/Info_nal_resultadospruebassaberpro_20202125.pdf" height="100%" width="100%"></object> -->
+              <iframe src="<?php echo URL_PATH;?>views/pdfs/Info_nal_resultadospruebassaberpro_20202125.pdf"
+        width="100%" height="500" frameborder="0"
+        allowfullscreen sandbox>
             </div>
          </div>
        </div>
